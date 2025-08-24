5ddbd3742d60de92743d6fe8f333b1478654f4cf
Sunia88 <sunia.raharja@gmail.com>
2025-08-24 23:04:40 +0800

    second commit

---
+++ src/main/java/com/suniacode/java/ecom/EcomMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.suniacode.java.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suniacode/java/ecom/EcomMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suniacode.java.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
