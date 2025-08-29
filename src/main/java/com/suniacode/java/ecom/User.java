3c86cfc8d68bd184e4f3c3db15b6290190b39981
Sunia88 <sunia.raharja@gmail.com>
2025-08-29 12:50:57 +0800

    Fourth Commit - Create User, User Controller and UserService

---
+++ src/main/java/com/suniacode/java/ecom/User.java
@@ -0,0 +1,10 @@
+package com.suniacode.java.ecom;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/suniacode/java/ecom/UserController.java
@@ -0,0 +1,29 @@
+package com.suniacode.java.ecom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        return userService.fetchAllUsers();
+    }
+
+    @PostMapping("/api/users")
+    public String createUser(@RequestBody User user) {
+        userService.addUser(user);
+        return "User added successfully";
+    }
+}
