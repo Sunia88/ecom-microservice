227a9ed27647e1c38c518e19fb61ffa6b4d2eb0e
Sunia88 <sunia.raharja@gmail.com>
2025-08-29 14:27:33 +0800

    Fifth Commit - Post User, Get Users and Get User By Id

--- src/main/java/com/suniacode/java/ecom/UserController.java
+++ src/main/java/com/suniacode/java/ecom/UserController.java
@@ -2,10 +2,7 @@ package com.suniacode.java.ecom;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +16,11 @@ public class UserController {
     @GetMapping("/api/users")
     public List<User> getAllUsers() {
         return userService.fetchAllUsers();
+    }
+
+    @GetMapping("/api/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.fetchUser(id);
     }
 
     @PostMapping("/api/users")
--- src/main/java/com/suniacode/java/ecom/UserService.java
+++ src/main/java/com/suniacode/java/ecom/UserService.java
@@ -8,13 +8,23 @@ import java.util.List;
 @Service
 public class UserService {
     private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
 
     public List<User> fetchAllUsers() {
         return userList;
     }
 
-    public List<User> addUser(User user) {
+    public void addUser(User user) {
+        user.setId(nextId++);
         userList.add(user);
-        return userList;
+    }
+
+    public User fetchUser(Long id) {
+        for (User user : userList) {
+            if (user.getId().equals(id)) {
+                return user;
+            }
+        }
+        return null;
     }
 }
