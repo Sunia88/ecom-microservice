1d3f50b1c09a8fe5d0f63f4990d1939a29f6b3f5
Sunia88 <sunia.raharja@gmail.com>
2025-08-29 14:58:43 +0800

    Sixth Commit - Response Entity

--- src/main/java/com/suniacode/java/ecom/UserController.java
+++ src/main/java/com/suniacode/java/ecom/UserController.java
@@ -2,6 +2,8 @@ package com.suniacode.java.ecom;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,18 +16,23 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers() {
-        return userService.fetchAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+        //Alternative way
+        //return ResponseEntity.ok(userService.fetchAllUsers());
     }
 
     @GetMapping("/api/users/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userService.fetchUser(id);
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        if (userService.fetchUser(id) == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(userService.fetchUser(id));
     }
 
     @PostMapping("/api/users")
-    public String createUser(@RequestBody User user) {
+    public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
-        return "User added successfully";
+        return ResponseEntity.ok("User added successfully");
     }
 }
