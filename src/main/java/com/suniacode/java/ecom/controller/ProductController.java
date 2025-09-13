d88abc9cfe8afc5577a7e62bfbb74952d19547cc
Sunia88 <sunia.raharja@gmail.com>
2025-09-13 18:59:17 +0800

    21 Commit - Creating EndPoints for Creating and Updating Product

---
+++ src/main/java/com/suniacode/java/ecom/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.suniacode.java.ecom.controller;
+
+import com.suniacode.java.ecom.dto.ProductRequest;
+import com.suniacode.java.ecom.dto.ProductResponse;
+import com.suniacode.java.ecom.repository.ProductRepository;
+import com.suniacode.java.ecom.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        return new ResponseEntity<ProductResponse>(productService.createProduct(productRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest)
+                .map(updatedProduct -> new ResponseEntity<>(updatedProduct, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
